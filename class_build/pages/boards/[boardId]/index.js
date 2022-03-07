1d382d3c6edd7a3ebdc4c65224bc4869e0f9bc1c
j00b33 <pjoobee@gmail.com>
2022-03-07 11:25:15 +0900

    DAY 35

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,3 +1,11 @@
+import { useRouter } from "next/router";
+
 export default function BoardsDetailPage() {
-  return <div>안녕하세요 게시글 상세페이지 입니다</div>;
+  const router = useRouter();
+  return (
+    <div>
+      안녕하세요 게시글 상세페이지 입니다, 게시글 아이디는{" "}
+      {router.query.boardId}
+    </div>
+  );
 }
