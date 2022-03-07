0ef1f7ba2ef60f190765722c2933c9851cc68106
j00b33 <pjoobee@gmail.com>
2022-03-07 13:21:30 +0900

    DAY35

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true, //slash를 기준으로 폴더 안에 폴더를 만들겠따
+  trailingSlash: true, //slash를 기준으로 폴더 안에 폴더를 만들겠다
+  generateBuildId: () => "jbeeshop.shop",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
